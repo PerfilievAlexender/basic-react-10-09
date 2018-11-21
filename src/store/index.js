cbb87488b1e1889dcff468fc252bdf7b2adeae4f
Alexender Perfiliev <perfiliev29@yandex.ru>
2018-11-21 20:50:55 +0300

    add redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
 import reduser from '../reduser'
+import thunk from 'redux-thunk'
 import randomId from '../middlewars/randomId'
 import api from '../middlewars/api'
 
-const store = createStore(reduser, applyMiddleware(randomId, api))
+const store = createStore(reduser, applyMiddleware(thunk, randomId, api))
 
 window.store = store
 

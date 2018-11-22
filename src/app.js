afd4886da2fdf8e686e1f63eb274365a26236b06
Alexender Perfiliev <perfiliev29@yandex.ru>
2018-11-22 22:02:52 +0300

    add navigation

--- src/app.js
+++ src/app.js
@@ -3,15 +3,31 @@ import ArticleList from './components/article-list'
 import SelectFilter from './components/filters/select'
 import DayPickerFilter from './components/filters/dayPicker'
 import Counter from './components/counter'
+import { Route, NavLink } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Counter />
-        <DayPickerFilter />
-        <SelectFilter />
-        <ArticleList />
+        <div>
+          <div>
+            <NavLink to="/counter">Counter</NavLink>
+          </div>
+          <div>
+            <NavLink to="/daySelect">Day select filter</NavLink>
+          </div>
+          <div>
+            <NavLink to="/select">Select filter</NavLink>
+          </div>
+          <div>
+            <NavLink to="/articles">Articles</NavLink>
+          </div>
+        </div>
+
+        <Route path="/counter" component={Counter} />
+        <Route path="/daySelect" component={DayPickerFilter} />
+        <Route path="/select" component={SelectFilter} />
+        <Route path="/articles" component={ArticleList} />
       </div>
     )
   }
